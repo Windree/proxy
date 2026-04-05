947b6eb6102c2eafed840bbc860e517992e8fbca
api <api>
2026-04-05 17:41:33 +0000

    feat: allow only local connections

--- ufw.sh
+++ ufw.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 source "$(dirname "${BASH_SOURCE[0]}")/.env"
-ufw allow "$PORT/tcp"
+set -Eeuo pipefail
+shopt -s inherit_errexit
+
+for remote in "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16" "fe80::/64"; do
+    ufw allow from "$remote" to any port "$XRAY_PORT" proto tcp
+done
+
+for remote in "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16" "fe80::/64"; do
+    ufw allow from "$remote" to any port "$WINDREE_TOR_PORT" proto tcp
+done
+
 ufw reload
